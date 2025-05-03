a1e5c5ac95e213a8e6e80f0bdafd9d7f3c9491bf
peipey11 <peipey11@users.noreply.github.com>
2025-05-04 00:31:37 +0530

    Added Level 1 layout and death animation logic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,17 +2,18 @@ extends CharacterBody2D
 
 # --------- VARIABLES ---------- #
 
-@export_category("Player Properties") # You can tweak these changes according to your likings
+@export_category("Player Properties")
 @export var move_speed : float = 400
 @export var jump_force : float = 600
 @export var gravity : float = 30
 @export var max_jump_count : int = 2
 var jump_count : int = 2
 
-@export_category("Toggle Functions") # Double jump feature is disable by default (Can be toggled from inspector)
-@export var double_jump : = false
+@export_category("Toggle Functions")
+@export var double_jump := false
 
 var is_grounded : bool = false
+var is_dead : bool = false  # Flag to prevent animation/movement when dead
 
 @onready var player_sprite = $AnimatedSprite2D
 @onready var spawn_point = %SpawnPoint
@@ -22,14 +23,13 @@ var is_grounded : bool = false
 # --------- BUILT-IN FUNCTIONS ---------- #
 
 func _process(_delta):
-	# Calling functions
-	movement()
-	player_animations()
-	flip_player()
-	
+	if not is_dead:
+		movement()
+		player_animations()
+		flip_player()
+
 # --------- CUSTOM FUNCTIONS ---------- #
 
-# <-- Player Movement Code -->
 func movement():
 	# Gravity
 	if !is_on_floor():
@@ -39,12 +39,10 @@ func movement():
 	
 	handle_jumping()
 	
-	# Move Player
 	var inputAxis = Input.get_axis("Left", "Right")
 	velocity = Vector2(inputAxis * move_speed, velocity.y)
 	move_and_slide()
 
-# Handles jumping functionality (double jump or single jump, can be toggled from inspector)
 func handle_jumping():
 	if Input.is_action_just_pressed("Jump"):
 		if is_on_floor() and !double_jump:
@@ -53,16 +51,17 @@ func handle_jumping():
 			jump()
 			jump_count -= 1
 
-# Player jump
 func jump():
 	jump_tween()
 	AudioManager.jump_sfx.play()
 	velocity.y = -jump_force
 
-# Handle Player Animations
 func player_animations():
+	if is_dead:
+		return  # Skip normal animations if dead
+
 	particle_trails.emitting = false
-	
+
 	if is_on_floor():
 		if abs(velocity.x) > 0:
 			particle_trails.emitting = true
@@ -72,15 +71,15 @@ func player_animations():
 	else:
 		player_sprite.play("Jump")
 
-# Flip player sprite based on X velocity
 func flip_player():
 	if velocity.x < 0: 
 		player_sprite.flip_h = true
 	elif velocity.x > 0:
 		player_sprite.flip_h = false
 
-# Tween Animations
 func death_tween():
+	is_dead = true  # Prevent other input/animations
+	player_sprite.play("Death")  # Play death animation
 	var tween = create_tween()
 	tween.tween_property(self, "scale", Vector2.ZERO, 0.15)
 	await tween.finished
@@ -91,8 +90,9 @@ func death_tween():
 
 func respawn_tween():
 	var tween = create_tween()
-	tween.stop(); tween.play()
-	tween.tween_property(self, "scale", Vector2.ONE, 0.15) 
+	tween.tween_property(self, "scale", Vector2.ONE, 0.15)
+	await tween.finished
+	is_dead = false  # Allow control again
 
 func jump_tween():
 	var tween = create_tween()
@@ -101,9 +101,8 @@ func jump_tween():
 
 # --------- SIGNALS ---------- #
 
-# Reset the player's position to the current level spawn point if collided with any trap
 func _on_collision_body_entered(_body):
-	if _body.is_in_group("Traps"):
+	if _body.is_in_group("Traps") and not is_dead:
 		AudioManager.death_sfx.play()
 		death_particles.emitting = true
-		death_tween()
+		await death_tween()
